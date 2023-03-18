c1ab387740bcbe848784ac05d262aa9670942f1c
ZhilnikovT <126979946+ZhilnikovT@users.noreply.github.com>
2023-03-18 23:42:05 +0300

    Update AliquotNumbers.java

--- src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
@@ -1,6 +1,16 @@
 package school.mjc.stage0.conditions.task3;
 
 public class AliquotNumbers {
+     public static void main(String[] args) {
+        AliquotNumbers g = new AliquotNumbers();
+        g.isFirstAliquot(40,10);
+    }
+    
     public void isFirstAliquot(int first, int second) {
+        if(second%first == 0){
+            System.out.println("Aliquot");
+        }else{
+            System.out.println("Not aliquot");
+        }
     }
 }
