20815d34ccf2cb5e3d2f582c2f5fa85b4bbcdf29
ZhilnikovT <126979946+ZhilnikovT@users.noreply.github.com>
2023-03-19 09:07:09 +0300

    Update DaysInMonthPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -1,13 +1,14 @@
 package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
-    public int[] c = new int[] {31,20,31,30,31,30,31,31,30,31,30,31};
+   
     public static void main(String[] args) {
         DaysInMonthPrinter g = new DaysInMonthPrinter();
-        g.amountOfDays(g, 11);
+        g.amountOfDays(11);
     }
-    public void amountOfDays(DaysInMonthPrinter g, int month) {
-        if(month >= 1 && month < 13) {System.out.println(g.c[month-1]);}
+    public void amountOfDays(int month) {
+        int[] c = new int[] {31,20,31,30,31,30,31,31,30,31,30,31};
+        if(month >= 1 && month < 13) {System.out.println(c[month-1]);}
         else{System.out.println("wrong number!");}
     }
 }
