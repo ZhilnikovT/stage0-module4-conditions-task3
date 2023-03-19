7bbd37c94babe9494fc3f6c07b4af982d2683643
ZhilnikovT <126979946+ZhilnikovT@users.noreply.github.com>
2023-03-19 08:14:00 +0300

    Update Divider.java

--- src/main/java/school/mjc/stage0/conditions/task3/Divider.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Divider.java
@@ -1,6 +1,20 @@
 package school.mjc.stage0.conditions.task3;
 
 public class Divider {
+     public static void main(String[] args) {
+        Divider g = new Divider();
+        g.isDividableBy5And11(1000);
+    }
     public void isDividableBy5And11(int number) {
+        if(number == 0){
+            System.out.println("cannot divide by zero");
+        }else{
+            if((number%5 + number%11) == 0){
+                System.out.println("Dividable");
+            }else{
+                System.out.println("Non-dividable");
+            }
+        }
     }
 }
+
