1b5c6c520f1a311aeab1c629f44d3864fdd5771e
ZhilnikovT <126979946+ZhilnikovT@users.noreply.github.com>
2023-03-19 08:04:54 +0300

    Update SalaryCalculator.java

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -1,6 +1,24 @@
 package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
+    public static void main(String[] args) {
+        SalaryCalculator g = new SalaryCalculator();
+        g.calculateSalary(1000);
+    }
+    
     public void calculateSalary(int salary) {
+        if(salary < 0){
+            System.out.println("wrong input!");
+        }else{
+            if(salary > 20000){
+                System.out.println((float)(salary * 0.8));
+            }else{
+                if(salary <= 20000 && salary > 10000){
+                    System.out.println((float)(salary * (1 - 0.18)));
+                }else{
+                    System.out.println((float)(salary * (1 - 0.15)));
+                }
+            }
+        }
     }
 }
